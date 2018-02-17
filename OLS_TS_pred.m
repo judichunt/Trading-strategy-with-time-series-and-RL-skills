847bd668044397799d9ab2b4e7a464aaaad294fe
judichunt <uioopoiuy@gmail.com>
2018-02-17 14:48:46 -0800

    Update OLS_TS_pred.m

--- OLS_TS_pred.m
+++ OLS_TS_pred.m
@@ -33,29 +33,5 @@ for i=1:MP/M
     Predict(:,i)=Predict_unsorted((i-1)*M+1:i*M);
 end
 
-% X_test=[];
-% Y_test=[];
-% for i=1:test_days-days
-%     X_test=cat(1,X_test, Test(:,i:i+num));
-%     Y_test=cat(1,Y_test, Test(:,i+num+days));
-%     %Y_test=cat(1,Y_test,prod(Test(:,i:i+num+days),2));
-% end
-% 
-% % size(Y_test)
-% % size(X_test)
-% % size(coeff)
-% 
-% 
-% SStot_test = sum((Y_test-mean(Y_test)).^2);
-% SSres_test = sum((Y_test-X_test*coeff).^2);
-% Rsquare_test = 1 - SSres_test/SStot_test;
-% 
-% mean_return = mean(Y_test./X_test(:,num+1));
-% 
-% SSlast_test = sum((Y_test-X_test(:,num+1)).^2);
-% SSpre_test = sum((Y_test-X_test(:,num+1)*mean_return).^2);
-% Rsquare_pre_test = 1 - SSres_test/SSpre_test;
-
-
 
 end
